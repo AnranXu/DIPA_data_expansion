f964b9bafa224e2f0d41530ce87da96ed33a9795
AnranXu <anran1998@126.com>
2023-01-26 11:56:49 +0900

    add python anova test (seems dof is wrong)

--- dataset/analyzer.py
+++ dataset/analyzer.py
@@ -2,6 +2,15 @@ import os
 import csv
 import json
 import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn import svm
+from sklearn import metrics
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+import torch
+import torch.nn as nn
 
 class analyzer:
     def __init__(self) -> None:
@@ -45,7 +54,7 @@ class analyzer:
                     category_name = self.code_openimage_map[cat]
                     self.openimages_mycat_map[category_name] = row[0]
 
-    def prepare_mega_table(self, mycat_mode = True)->None:
+    def prepare_mega_table(self, mycat_mode = True, save_csv = False)->None:
         #mycat_mode: only aggregate annotations that can be summarized in mycat (also store them in mycat in mega_table).
         #the mega table includes all privacy annotations with all corresponding info (three metrics, big five, age, gender, platform)
 
@@ -106,8 +115,49 @@ class analyzer:
                         })
 
                         self.mega_table = pd.concat([self.mega_table, entry], ignore_index=True)
+        if save_csv:
+            self.mega_table.to_csv('./mega_table.csv', index =False)
+
+    def svm(self, input_channel, output_channel, read_csv = False) -> None:
+        if read_csv:
+            self.mega_table = pd.read_csv('./mega_table.csv')
+        else:
+            self.prepare_mega_table()
+        self.mega_table['category'] = encoder.fit_transform(self.mega_table['category'])
+        self.mega_table['gender'] = encoder.fit_transform(self.mega_table['gender'])
+        self.mega_table['platform'] = encoder.fit_transform(self.mega_table['platform'])
+        scaler = StandardScaler()
+        encoder = LabelEncoder()
+        X = self.mega_table[input_channel].values
+        y = self.mega_table[output_channel].values
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.80, random_state=0)
+        X_train = scaler.fit_transform(X_train)
+        X_test = scaler.transform(X_test)
+        classifier=svm.SVC(kernel='rbf',gamma=0.1,decision_function_shape='ovo',C=0.8)
+        classifier.fit(X_train,y_train)
+        y_pred = classifier.predict(X_test)
+
+        # Print evaluation metrics
+        print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
+        print("Precision:",metrics.precision_score(y_test, y_pred,average='weighted'))
+        print("Recall:",metrics.recall_score(y_test, y_pred,average='weighted'))
+
+    def anova(self,read_csv = False) -> None:
+        ## the degree of freedom of "informativeness" is wrong, it should be 6 rather than 1
+        ## I am using R to perform this
+        if read_csv:
+            self.mega_table = pd.read_csv('./mega_table.csv')
+        else:
+            self.prepare_mega_table()
+        print(self.mega_table['informativeness'].unique())
+        model = ols('sharing ~ category*informativeness', data=self.mega_table).fit()
+        aov_table = sm.stats.anova_lm(model, typ=1)
+        print(aov_table)
 
 if __name__ == '__main__':
     analyze = analyzer()
-    analyze.prepare_mega_table()
-    print(analyze.mega_table)
+    input_channel = ['category', 'reason', 'informativeness']
+    output_channel = ['sharing']
+    #analyze.svm(input_channel, output_channel, read_csv=True)
+    #analyze.anova(True)
+    
