2d85c4970e96ee1bbfc0b34774810c325ca06cc8
AnranXu <anran1998@126.com>
2023-02-15 17:52:02 +0900

    Update inference_model.py

--- dataset/inference_model.py
+++ dataset/inference_model.py
@@ -97,7 +97,7 @@ class BaseModel(pl.LightningModule):
             conf.append(np.zeros((output_dim,output_dim)))
         for i, (output_name, output_dim) in enumerate(self.output_channel.items()):
             if output_name == 'informativeness':
-                distance += l1_distance_loss(y[:, i].detach().cpu().numpy(), y_preds[i].detach().cpu().numpy())
+                distance += l1_distance_loss(y[:, i].detach().cpu().numpy(), max_indices.detach().numpy())
             _, max_indices = torch.max(y_preds[i], dim = 1)
             acc[i] = metrics.accuracy_score(y[:,i].detach().cpu().numpy(), max_indices.detach().cpu().numpy())
             pre[i] = metrics.precision_score(y[:,i].detach().cpu().numpy(), max_indices.detach().cpu().numpy(),average='weighted')
@@ -109,7 +109,7 @@ class BaseModel(pl.LightningModule):
         df = pd.DataFrame(pandas_data, index=self.output_channel.keys())
         print(df.round(3))
         if 'informativeness' in self.output_channel.keys():
-            print('informativenss distance: ', distance * 6)
+            print('informativenss distance: ', distance)
         for i, (output_name, output_dim) in enumerate(self.output_channel.items()): 
             if output_name == 'informativeness':
                 self.log("val/distance for {}".format(output_name), distance)
