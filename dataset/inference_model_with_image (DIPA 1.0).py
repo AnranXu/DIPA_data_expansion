b67321b617145ba28c6cb2f505b680ac97238e78
AnranXu <anran1998@126.com>
2023-02-17 11:08:16 +0900

    change save dir

--- dataset/inference_model_with_image (DIPA 1.0).py
+++ dataset/inference_model_with_image (DIPA 1.0).py
@@ -65,8 +65,8 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_dataset, batch_size=20, generator=torch.Generator(device='cuda'), shuffle=True)
     val_loader = DataLoader(val_dataset, generator=torch.Generator(device='cuda'), batch_size=20)
     
-    wandb_logger = WandbLogger(project="DIPA-inference", name = 'mix losses without dropout (resnet18)')
-    checkpoint_callback = ModelCheckpoint(dirpath='./models/test mix losses with augmentation (resnet18)/', save_last=True, monitor='val loss')
+    wandb_logger = WandbLogger(project="DIPA-inference", name = 'mix losses with original labels (mobilenet v3 large)')
+    checkpoint_callback = ModelCheckpoint(dirpath='./models/mix losses with original labels (mobilenet v3 large)/', save_last=True, monitor='val loss')
 
     trainer = pl.Trainer(accelerator='gpu', devices=[0],logger=wandb_logger, 
     auto_lr_find=True, max_epochs = 300, callbacks=[checkpoint_callback])
