b4dcf8bcc75140b1530bb9cc8128fe10e1944121
AnranXu <anran1998@126.com>
2023-02-20 14:33:18 +0900

    Update inference_dataset.py

--- dataset/inference_dataset.py
+++ dataset/inference_dataset.py
@@ -59,7 +59,7 @@ class ImageMaskDataset(Dataset):
 
         mask = torch.zeros((self.input_dim, self.image_size[0], self.image_size[1]))
         for i, input_name in enumerate(self.input_vector):
-            tot_num = len(mega_table[input_name].unique())
+            tot_num = len(self.mega_table[input_name].unique())
             for x, y, w, h in bboxes:
                 x, y, w, h = int(x), int(y), int(w), int(h)
                 mask[i, y:y+h, x:x+w] = self.mega_table[input_name].iloc[idx] / tot_num
