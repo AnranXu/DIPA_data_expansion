9fa5730d33eda5dc2791a229c614b85f78c87d49
AnranXu <anran1998@126.com>
2023-02-22 23:22:27 +0900

    Update inference_model_with_image (DIPA 1.0).py

--- dataset/inference_model_with_image (DIPA 1.0).py
+++ dataset/inference_model_with_image (DIPA 1.0).py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
     train_loader = DataLoader(train_dataset, batch_size=96, generator=torch.Generator(device='cuda'), shuffle=True)
     val_loader = DataLoader(val_dataset, generator=torch.Generator(device='cuda'), batch_size=64)
     
-    wandb_logger = WandbLogger(project="DIPA-inference", name = 'mix losses new mask (resnet 50)')
-    checkpoint_callback = ModelCheckpoint(dirpath='./models/mix losses new mask (resnet 50)/', save_last=True, monitor='val loss')
+    wandb_logger = WandbLogger(project="DIPA-inference", name = 'mix losses macro (resnet 50)')
+    checkpoint_callback = ModelCheckpoint(dirpath='./models/mix losses macro (resnet 50)/', save_last=True, monitor='val loss')
 
     trainer = pl.Trainer(accelerator='gpu', devices=[1],logger=wandb_logger, 
     auto_lr_find=True, max_epochs = 300, callbacks=[checkpoint_callback])
