1dc0f13f40c79071af74ed63abe33e1ba336997b
AnranXu <anran1998@126.com>
2023-02-26 00:31:19 +0900

    Update inference_model_with_image (DIPA 2.0).py

--- results/inference_model_with_image (DIPA 2.0).py
+++ results/inference_model_with_image (DIPA 2.0).py
@@ -76,7 +76,7 @@ if __name__ == '__main__':
     train_dataset = ImageMaskDataset(train_df, image_folder, label_folder, input_channel, image_size, flip = True)
     val_dataset = ImageMaskDataset(val_df, image_folder, label_folder, input_channel, image_size)    
 
-    train_loader = DataLoader(train_dataset, batch_size=96, generator=torch.Generator(device='cuda'), shuffle=True)
+    train_loader = DataLoader(train_dataset, batch_size=64, generator=torch.Generator(device='cuda'), shuffle=True)
     val_loader = DataLoader(val_dataset, generator=torch.Generator(device='cuda'), batch_size=64)
     
     wandb_logger = WandbLogger(project="DIPA2.0-inference test (uncompleted collection)", name = 'mix losses sigmoid (Resnet 50)')
