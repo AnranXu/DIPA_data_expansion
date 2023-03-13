67a59a980c3df2f769bad441825874999a610d60
AnranXu <anran1998@126.com>
2023-03-13 11:47:25 +0900

    Update inference_model_with_image (DIPA 1.0).py

--- dataset/inference_model_with_image (DIPA 1.0).py
+++ dataset/inference_model_with_image (DIPA 1.0).py
@@ -69,12 +69,12 @@ if __name__ == '__main__':
     val_dataset = ImageMaskDataset(val_df, image_folder, label_folder, input_channel, output_name, image_size)    
 
     train_loader = DataLoader(train_dataset, batch_size=96, generator=torch.Generator(device='cuda'), shuffle=True)
-    val_loader = DataLoader(val_dataset, generator=torch.Generator(device='cuda'), batch_size=64)
+    val_loader = DataLoader(val_dataset, generator=torch.Generator(device='cuda'), batch_size=32)
     
     wandb_logger = WandbLogger(project="DIPA2.0-inference test", name = 'DIPA 1.0: Include prev manual label  (resnet 50)')
     checkpoint_callback = ModelCheckpoint(dirpath='./models/DIPA 1.0: Include prev manual label (resnet 50)/', save_last=True, monitor='val loss')
 
-    trainer = pl.Trainer(accelerator='gpu', devices=[1],logger=wandb_logger, 
+    trainer = pl.Trainer(accelerator='gpu', devices=[0],logger=wandb_logger, 
     auto_lr_find=True, max_epochs = 100, callbacks=[checkpoint_callback])
     lr_finder = trainer.tuner.lr_find(model, train_loader)
     model.hparams.learning_rate = lr_finder.suggestion()
